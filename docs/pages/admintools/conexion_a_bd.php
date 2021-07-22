b3623ca33d9f2c1c5762a5fa5e135f2841fb627c
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2021-07-22 14:44:46 -0500

    FIX DATABASE

--- docs/pages/admintools/conexion_a_bd.php
+++
@@ -1,8 +0,0 @@
- <?php
-
-    $conn = mysqli_connect("localhost", "root", "root", "SVE_BD");
-    if(!$conn)
-        echo "Hubo un error al conectarse";
-        
-        
-  ?>
--- docs/pages/admintools/get_stud.php
+++ docs/pages/admintools/get_stud.php
@@ -1,26 +1,23 @@
 <?php 
+
     $conn = mysqli_connect("localhost", "root", "root", "SVE_BD");
-    if(!$conn){
-    echo "Hubo un error al conectarse a la bd";
-    exit();
-    }
-    
+if(!conn) {
+    echo "cant connect";
+}
+    $SQL = "SELECT JSON_OBJECT('nombre', nombres, 'apellidos', 
+    apellidos, 'nacionalidad', nacionalidad, 'etnia', etnia, 
+    'estcivil', estado_civil) from infoestud WHERE id_est_ced=".$_REQUEST["cd"];
     
 
-    $SQL = "SELECT JSON_OBJECT('nombre', nombres, 'apellidos', apellidos, 'nacionalidad', nacionalidad, 'etnia', etnia, 'estcivil', estado_civil) from infoestud WHERE id_est_ced=".$_REQUEST["ced"];
     $ans = $conn->query($SQL);
-    
     if($ans->num_rows > 0) {
         $row = $ans->fetch_array();
         echo $row[0];
     }
     else {
-        echo "Not found";
-    } 
-        
-        
+        echo "Not found :(";
+    }
 
-   
+
 ?>
-
 
