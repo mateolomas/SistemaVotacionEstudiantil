727d3d4defbc84cdd3101d9c3052450a38ffde5f
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2021-07-22 23:31:02 -0500

    FIX NAVBAR

--- docs/pages/dashboard.php
+++ docs/pages/dashboard.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
-    <link rel="stylesheet" href="../styles/styles.css">
+    <link rel="stylesheet" href="../styles/navstyle.css">
+    <link rel="stylesheet" href="../styles/styles_dash.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/6b41f4b4ea.js" crossorigin="anonymous"></script>
-    <script rel="script" src="../scripts/jquery-3.6.0.min.js"></script>
+    
 
     <script rel="script" src="../scripts/script.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -41,70 +42,36 @@
       }
     </script>
 </head>
-<body id="main" onload="expandNav()">
-    <header>
-        <div id="whiteheader">
-            <div><input id="search" placeholder="Necesitas ayuda?" style="font-weight: lighter; color: rgba(128, 128, 128, 0.712); width: 30%; border: none; border-radius: 15px; padding: 8px; background-color: rgba(128, 128, 128, 0.194)"></div>
+<body>
 
-        </div>
-        
-    </header>
-    <span id="dash">Dashboard</span><br>
-    
-        <div id="sidebar">
-            <ul>
-              <li><div><img id="sidebar-header" src="../media/yachay_verde.png"></div></li>
-              <li><a href="dashboard.html">Dashboard</a></li>
-              <li><a href="candidatos.html">Candidatos</a></li>
-              <li><a href="votarya.php">Votar Ya</a></li>
-              <li><a href="estadisticas.html">Estadisticas</a></li>
-              <li><a href="certificado.html">Certificado de Votacion</a></li>
-              <li><div id="user">
-                <span>Mateo Lomas</span>
-                <span>Estudiante Regular</span>
-                <img id="imguser" src="../media/userdefault.png">
-            </div></li> 
-            </ul>
-        
-            <div id="sidebar-btn" onclick='toggleNav()'>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
+      <nav class="navbar">
+              <ul class="navbar-nav">
+                <li class="logo"><a class="nav-link" href="#"><img id="ilogo" src="../media/yachay_verde.png"></a></li>
+                <li class="nav-item"><a class="nav-link" href="dashboard.php">     <i class="fas fa-home icons"></i>   <span class="link-text">Dashboard</span>    </a></li>
+                <li class="nav-item"><a class="nav-link" href="candidatos.html">   <i class="fas fa-user-friends icons"></i> <span class="link-text">Candidatos</span>   </a></li>
+                <li class="nav-item"><a class="nav-link" href="votarya.php">       <i class="fas fa-vote-yea icons"></i> <span class="link-text">Vota ya!</span>     </a></li>
+                <li class="nav-item"><a class="nav-link" href="estadisticas.html"> <i class="fas fa-poll icons"></i>     <span class="link-text">Estadisticas</span> </a></li>
+                <li class="nav-item"><a class="nav-link" href="certificado.html">  <i class="far fa-id-card icons"></i>  <span class="link-text">Certificado</span>  </a></li>
 
-            
-        
-          </div>
-          
-        <div id="iconsidebar">
-            <ul>
+                <li class="nav-item"><a class="nav-link" href="usuario.html">  <i class="fas fa-user-circle"></i>  <span class="link-text">Mateo Lomas</span>  </a></li>
                 
-                <li><span></span></li>
-                <li><span></span></li>
-                
-                
-                <br>
-                
-                <li><a href=""><i class="fas fa-home"></i></a></li>
-                
-                <li><a href=""><i class="fas fa-user-friends"></i></a></li>
-                <li><a href=""><i class="fas fa-vote-yea"></i></a></li>
-                <li><a href=""><i class="fas fa-poll"></i></a></li>
-                <li><a href=""><i class="far fa-id-card"></i></a></li>
-                 
+  
               </ul>
+        </nav>
 
-        </div>
-       
-            
+        <header>
+        
+            <input id="searchbox" placeholder="¿Necesitas ayuda?">
+          
+        </header>
+        
         
     
+    <main>
+      
+    <?php
     
-    <div class="container">
     
-    
-    
-    <?php 
     $ans = $conn->query("SELECT COUNT(*) as nro_Estud from infoestud");
     $info = $ans->fetch_array();
     
@@ -116,34 +83,37 @@
 
 
     
-    echo "<div id='candidates' class='quickstats'>
-      <img style='width: 50px; border: 1; position: relative;' src='/docs/media/icons/153-bar-chart-growth-outline.gif'>
+    echo "
+      <div class='container_stats'>
+    
+      <div class='quickstats'>
+    <img src='/docs/media/icons/153-bar-chart-growth-outline.gif'>
       <span>".$info['nro_Estud']."</span>
-      <span>Inscritas en el patron electoral.</span>
-    </div>
-    
-    <div id='votes' class='quickstats'>
-    <img style='width: 50px;' src='/docs/media/icons/17-avatar-man-nodding-outline.gif'>
+      <p> Inscritas en el patron electoral.</p>
+        </div>
+     
+    <div class='quickstats'>
+   <img src='/docs/media/icons/17-avatar-man-nodding-outline.gif'>
     <span>".$vote_already['already']."</span>
-    
-    <span>Han votado</span>
+    <p> Han votado</p>
 </div>
 
-<div id='votesleft' class='quickstats'>
-    <img style='width: 50px;' src='/docs/media/icons/69-eye-outline.gif'>
+<div class='quickstats'>
+    <img src='/docs/media/icons/69-eye-outline.gif'>
     <span>".$vote_left['noready']."</span>
-    <span>Ausencia</span>
+    <p> Ausencia</p>
+</div> 
+
 </div>
 "
     ?>
 
-
- 
-        
-    
     <div id="graph">
-        <div id="barchart_material" style="width: 900px; height: 500px;"></div>
+        <div id="barchart_material"></div>
         
     </div> 
+  
+</main>
+ 
 </body>
 </html>
