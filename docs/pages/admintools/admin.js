1cb5aee79cf8e8627dddd3d6534dccd8afad6090
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2021-07-27 20:46:11 -0500

    ADD BUTTONS ADMIN PANEL

--- docs/pages/admintools/admin.js
+++ docs/pages/admintools/admin.js
@@ -13,9 +13,78 @@ function buscarAl() {
     }
     xhttp.open("GET", "get_stud.php?cd="+document.getElementById("icedula").value, true);
     xhttp.send();
+
+
     }
     
     
           //xhttp.open("POST", "get_stud.php", true);
           //xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
           //xhttp.send("ced="+ document.getElementById("icedula").value);
+
+          function leerf () {
+            document.getElementById("buscar").style.display = "none";
+            document.getElementById("anadir").style.display = "none";
+            document.getElementById("leer").style.display = "block";
+            document.getElementById("actualizar").style.display = "none";
+            document.getElementById("Eliminar").style.display = "none";
+            document.getElementById("cargarExcel").style.display = "none";
+          }
+
+function findf () {
+    document.getElementById("buscar").style.display = "block";
+document.getElementById("anadir").style.display = "none";
+document.getElementById("leer").style.display = "none";
+document.getElementById("actualizar").style.display = "none";
+document.getElementById("Eliminar").style.display = "none";
+document.getElementById("cargarExcel").style.display = "none";
+
+}
+function anadirf () {
+    document.getElementById("buscar").style.display = "none";
+    document.getElementById("anadir").style.display = "block";
+    document.getElementById("leer").style.display = "none";
+    document.getElementById("actualizar").style.display = "none";
+    document.getElementById("Eliminar").style.display = "none";
+    document.getElementById("cargarExcel").style.display = "none";
+    
+    }
+    function actualf () {
+        document.getElementById("buscar").style.display = "none";
+        document.getElementById("anadir").style.display = "none";
+        document.getElementById("leer").style.display = "none";
+        document.getElementById("actualizar").style.display = "block";
+        document.getElementById("Eliminar").style.display = "none";
+        document.getElementById("cargarExcel").style.display = "none";
+        }
+
+        function eliminarf () {
+            document.getElementById("buscar").style.display = "none";
+            document.getElementById("anadir").style.display = "none";
+            document.getElementById("leer").style.display = "none";
+            document.getElementById("actualizar").style.display = "none";
+            document.getElementById("Eliminar").style.display = "block";
+            document.getElementById("cargarExcel").style.display = "none";
+            }
+
+
+function hidemain () {
+    document.getElementById("buscar").style.display = "none";
+            document.getElementById("anadir").style.display = "none";
+            document.getElementById("leer").style.display = "none";
+            document.getElementById("actualizar").style.display = "none";
+            document.getElementById("Eliminar").style.display = "none";
+            document.getElementById("cargarExcel").style.display = "none";
+
+}
+
+
+function cargarf(){
+    document.getElementById("buscar").style.display = "none";
+            document.getElementById("anadir").style.display = "none";
+            document.getElementById("leer").style.display = "none";
+            document.getElementById("actualizar").style.display = "none";
+            document.getElementById("Eliminar").style.display = "none";
+            document.getElementById("cargarExcel").style.display = "none";
+
+}
