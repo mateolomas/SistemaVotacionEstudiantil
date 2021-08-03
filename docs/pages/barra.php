98974635f196d434b7d1ff1f5af6eebd0cbf2ebe
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2021-08-02 21:22:38 -0500

    add cronograma

--- docs/pages/barra.php
+++ docs/pages/barra.php
@@ -3,16 +3,26 @@
       <li class="logo"><a class="nav-link" href="#"><img id="ilogo" src="../media/yachay_verde.png"></a></li>
       <li class="nav-item"><a class="nav-link" href="dashboard.php">     <i class="fas fa-home icons"></i>   <span class="link-text">Dashboard</span>    </a></li>
       <li class="nav-item"><a class="nav-link" href="candidatos.php">   <i class="fas fa-user-friends icons"></i> <span class="link-text">Candidatos</span>   </a></li>
+
+      <li class="nav-item"><a class="nav-link" href="cronograma.html">  <i class="fas fa-calendar-alt"></i>  <span class="link-text">Cronograma</span>  </a></li>
+
+
       <li class="nav-item"><a class="nav-link" href="votarya.php">       <i class="fas fa-vote-yea icons"></i> <span class="link-text">Vota ya!</span>     </a></li>
       <li class="nav-item"><a class="nav-link" href="estadisticas.php"> <i class="fas fa-poll icons"></i>     <span class="link-text">Estadisticas</span> </a></li>
+
+
       <li class="nav-item"><a class="nav-link" href="certificado.php">  <i class="far fa-id-card icons"></i>  <span class="link-text">Certificado</span>  </a></li>
+
+
       <?php 
       session_start();
         echo "<li class='nav-item'><a class='nav-link' href='usuario.html'>  <i class='fas fa-user-circle'></i>  <span class='link-text' id='usuario_actual'>".$_SESSION['name']." ".$_SESSION['apel']."</span>  </a></li>";
       ?>
       <li class="nav-item"><a class="nav-link" href="../../../logout.php">  <i class="fas fa-sign-out-alt"></i>  <span class="link-text">Logout</span>  </a></li>
+
       
-      
+
+
 
     </ul>
 </nav>
